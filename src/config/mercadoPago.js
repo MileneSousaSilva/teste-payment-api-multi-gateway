eccb1dfed99da2465cb42164235bff24d835131f
MileneSousaSilva <milene.sousa.silvaa@gmail.com>
2026-03-12 16:46:26 -0300

    changes

--- src/config/mercadoPago.js
+++ src/config/mercadoPago.js
@@ -2,7 +2,7 @@
 const { MercadoPagoConfig } = require("mercadopago");
 
 const client = new MercadoPagoConfig({
-  accessToken: "APP_USR-1439153509143048-031210-0e8debd24fd64dfa081112556a5cdbd6-3260578921"
+  accessToken: "token_de_acceso",
 });
 
 module.exports = client;
